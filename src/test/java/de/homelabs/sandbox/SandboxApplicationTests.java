2d52cc3e744edcf7c01072ea3410f220ebddbd34
sm1rk <smirk.mueller@gmail.com>
2025-01-20 20:01:01 +0100

    added tests

--- src/test/java/de/homelabs/sandbox/SandboxApplicationTests.java
+++ src/test/java/de/homelabs/sandbox/SandboxApplicationTests.java
@@ -1,13 +1,34 @@
 package de.homelabs.sandbox;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import de.homelabs.sandbox.controller.MainController;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SandboxApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+	
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Autowired
+	MainController mainController;
+	
 	@Test
-	void contextLoads() {
+	void testMainController() {
+		assertThat(mainController).isNotNull();
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class)).contains("Hello World!");
 	}
 
 }
